0fcbe21e9020426385bb2a832dfae0803357d4c8
Nathan Webb <nathanwebb.j.h@gmail.com>
2018-12-19 17:17:05 +0000

    Fine tuning basic features, core funcationality working. Next is to start on spec requirements.

--- BankServer.py
+++ BankServer.py
@@ -4,7 +4,7 @@ import sys
 
 class ThreadedServer(object):
     def __init__(self, host, port):
-        self.host = '127.0.0.1'
+        self.host = host
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -13,31 +13,36 @@ class ThreadedServer(object):
     def listen(self):
         self.sock.listen(5)
         while True:
+            print("Listening for clients")
             client, address = self.sock.accept()
             #client.settimeout(60)
-            print("New client connected")
             threading.Thread(target = self.listenToClient,args = (client,address)).start()
 
     def listenToClient(self, client, address):
+        print("Client connected %s" % str(address))
         buffer = 1024
         while True:
             try:
-                message = "hello"
-                #print(address)
-                client.send(message.encode('ASCII'))
-                #data = bytes(client.recv(buffer))
-                #if data:
-                    #print(data + '%s'%data.decode('ASCII'))
-                    #break
-                #else:
-                    #print(sys.exc_info()[0])
-                    #raise error('Client disconnected')
-                client.close()
+                data = bytes(client.recv(buffer))
+                if data:
+                    message = "Client  %s  connected" % str(address)
+                    self.readMessage(client, address, data)
+                    self.sendMessage(client, message)
             except:
+                print("Client %s disconnected" % str(address))
                 client.close()
+                print(sys.exc_info()[0])
                 return False
 
+    def readMessage(self, client, address, data):
+        command = data.decode('ASCII')
+        print(str(address) + " " + command)
+
+    def sendMessage(self, client, data):
+        client.send(data.encode('ASCII'))
+
 if __name__ == "__main__":
-    serverIP = '127.0.0.1'
+    serverIP = 'localhost'
     port_num = 5005
+    print("Server starting")
     ThreadedServer(serverIP,port_num).listen()
